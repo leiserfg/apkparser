0cad7c7bb2a5a345eaa1a98a77f4ff081839aed7
leiserfg <leiserfg@gmail.com>
2018-09-07 11:26:31 -0400

    Update version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.0",
+    version="1.1",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "cairosvg"],
 )
