ef0e0baaba7c83a346bb9a8277d317aea6df0cda
leiserfg <leiserfg@gmail.com>
2018-09-10 14:32:33 -0400

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.2",
+    version="1.3",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "cairosvg"],
 )
