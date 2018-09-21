f1a5cf5b4e8f0c65cc07e6480cf93542cbee0d68
leiserfg <leiserfg@gmail.com>
2018-09-21 16:26:04 -0400

    Upgrade to 1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.6",
+    version="1.7",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "wand"],
 )
