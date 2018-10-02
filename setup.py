f896d8f432d676ab8f0fb9f987d1dc15733d3a0c
leiserfg <leiserfg@gmail.com>
2018-10-02 11:02:50 -0400

    Increse version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.8",
+    version="1.9",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "wand"],
 )
