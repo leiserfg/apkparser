1f06dfc8a6c51df03350165b256d81ab7c49fb62
leiserfg <leiserfg@gmail.com>
2019-07-14 11:39:48 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.9.3",
+    version="1.9.4",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "wand"],
 )
