5ff078518675e704d44f2add3c26195cc0dd483d
leiserfg <leiserfg@gmail.com>
2019-07-14 17:19:15 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="apkparser",
-    version="1.9.4",
+    version="1.10.0",
     packages=["apkparser"],
     install_requires=["pyasn1", "cryptography", "lxml", "Pillow", "wand"],
 )
